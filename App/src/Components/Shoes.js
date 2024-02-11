3ed7f48df583b01108a136e5dbd91a3e2fa7f81b
mwangiowen <owen31588@gmail.com>
2024-02-11 23:26:36 +0300

    copy 1 file, update 2 files and create 1 file

--- App/src/Components/Shoes.js
+++ App/src/Components/Shoes.js
@@ -1,94 +1,42 @@
-import { Button } from "bootstrap";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./Searchbar";
+import "./shoes.css";
 
 function Shoes() {
   const [shoes, setShoes] = useState([]);
 
   useEffect(() => {
-    fetch('/displayshoes')
+    fetch("/displayshoes")
       .then((response) => response.json())
       .then((data) => setShoes(data));
   }, []);
 
   return (
     <div className="container">
-      <h1>Shue Store</h1>
-      <SearchBar shoes={shoes}/>
-      <div>
+      <h1>Shoe Store</h1>
+      <SearchBar shoes={shoes} />
+      <div className="shoe-list">
         {shoes.map((shoe) => (
-          <li key={shoe.id}>
+          <div key={shoe.id} className="shoe-item">
             <Link to={`/shoes/${shoe.id}`}>
-              <div className='image-container'>
-                <img src={shoe.image_url} alt={shoe.name} />
-                <div className="image-text">
-                  Click for shoe info
-                </div>
+              <div className="image-container">
+                <img src={shoe.image} alt={shoe.name} />
+                <div className="image-text">Click for shoe info</div>
               </div>
-              </Link>
-              <p>Brand:{shoe.brand}</p>
-              <strong> Name:{shoe.name}</strong>
-              <p> Price:{shoe.price}</p>
-              <p>Sizes:{shoe.sizes}</p>
-            
-          </li>
+            </Link>
+            <p>Brand: {shoe.brand}</p>
+            <strong>Name: {shoe.name}</strong>
+            <p>Price: {shoe.price}</p>
+            <p>Sizes: {shoe.sizes}</p>
+          </div>
         ))}
       </div>
-
-      <button><Link to="/add">Add</Link></button>
+      <button>
+        <Link to="/add">Add</Link>
+      </button>
     </div>
   );
 }
 
 export default Shoes;
-// import { Button } from "bootstrap";
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import SearchBar from "./Searchbar";
-
-// function Shoes() {
-//   const [shoes, setShoes] = useState([]);
-//   const [searchTerm, setSearchTerm] = useState('');
-
-//   useEffect(() => {
-//     fetch('/displayshoes')
-//       .then((response) => response.json())
-//       .then((data) => setShoes(data));
-//   }, []);
-
-//   const filteredShoes = shoes.filter((shoe) =>
-//     shoe.description && shoe.description.toLowerCase().includes(searchTerm.toLowerCase())
-//   );
-//   filteredShoes.sort((a, b) => a.description.localeCompare(b.description));
-
-//   return (
-//     <div className="container">
-//       <h1>Shoe Store</h1>
-//       {/* <SearchBar shoes={shoes} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> */}
-//       <ul>
-//         {filteredShoes.map((shoe) => (
-//           <li key={shoe.id}>
-//             <Link to={`/shoes/${shoe.id}`}>
-//               <div className='image-container'>
-//                 <img src={shoe.image_url} alt={shoe.name} />
-//                 <div className="image-text">
-//                   Click for shoe info
-//                 </div>
-//               </div>
-//             </Link>
-//             <p>Brand: {shoe.brand}</p>
-//             <strong>Name: {shoe.name}</strong>
-//             <p>Price: {shoe.price}</p>
-//             <p>Sizes: {shoe.sizes}</p>
-//           </li>
-//         ))}
-//       </ul>
-
-//       <button><Link to="/add">Add</Link></button>
-//     </div>
-//   );
-// }
-
-// export default Shoes;
-
